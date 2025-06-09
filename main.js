9ea9230444dc60632c59d436cba66a0d4fa3619d
HAPPYCAT <charlottechoibk@gmail.com>
2025-06-09 23:59:12 +0800

    working-adding-queryHoursperWeek

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-const options = {
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    timeZone: 'America/New_York' // Specify Eastern Time
-};
+const time = Date.parse("2025-06-09")
+var d = new Date(time); 
+const sunday = new Date(time); 
+const saturday = new Date(time); 
+// console.log(`the date of today is ${d};`)
+var x = d.getDay();
+var y = 6-d.getDay();
+sunday.setDate(d.getDate() - x);
+saturday.setDate(d.getDate() + y)
+const sun = sunday.toISOString().split('T')[0]
+const sat = saturday.toISOString().split('T')[0]
+// console.log(`the date of the Sunday of this week is ${sun}.`)
+// console.log(`the date of the Saturday of this week is ${sat}.`)
 
-const displayString = `Start Date & Time: ${new 
-    Date(Date.now()).toLocaleString('America/N')}`;
-    
-console.log(displayString)
+// const test = new Date(Date.parse("2025-06-09"))
+// console.log(test.getDate())
+
+console.log(sat===sun)
--- script.js
+++ script.js
@@ -27,8 +27,8 @@ const options = {
     timeZone: 'America/New_York' // Specify Eastern Time
 };
 
-class Session{
-    constructor(elapsedTime, sessionStart){
+class Session {
+    constructor(elapsedTime, sessionStart) {
         const dateObj = new Date(sessionStart)
         this.id = dateObj.toISOString()
         this.elapsedTime = elapsedTime;
@@ -41,89 +41,124 @@ const versionNum = 1; //this must be int positive
 const databaseName = "WorkHourCounterDB";
 const storeName = "sessions";
 
-function openDB(modificationToDB){
-    const dbPromise = indexedDB.open(databaseName,versionNum);
+function openDB(modificationToDB) {
+    const dbPromise = indexedDB.open(databaseName, versionNum);
     let db;
-    
-    dbPromise.onupgradeneeded = (event)=>{
+
+    dbPromise.onupgradeneeded = (event) => {
         db = event.target.result;
-    
-        if(!db.objectStoreNames.contain(storeName)){
+
+        if (!db.objectStoreNames.contain(storeName)) {
             // id should be existing field
             // id is unique because it is the time and date of the session
-            const store = db.createObjectStore(storeName, {keyPath: "id"});
-        
+            const store = db.createObjectStore(storeName, { keyPath: "id" });
+
             // Create an index on startTimestamp so we can query by date easily later.
             store.createIndex("byDate", "date", { unique: false });
         }
     }
-    
-    dbPromise.onerror = function(event) {
+
+    dbPromise.onerror = function (event) {
         console.error("IndexedDB open error:", event.target.error);
     };
-      
-    dbPromise.onsuccess = function(event) {
+
+    dbPromise.onsuccess = function (event) {
         //event is an instance of indexedDB database
         console.log("Database opened successfully");
         db = event.target.result;
 
         modificationToDB(db);
-    
-        db.onerror = (event)=>{
+
+        db.onerror = (event) => {
             //generic error handler for all database errors 
             console.error(`Database error: ${event.target.error?.message}.`)
         }
-    }; 
+    };
 }
 
-function addtoDB(elapsedTime, sessionStart){
-    function modificationToDB(db){
-        const obj = new Session(elapsedTime,sessionStart);
+function addtoDB(elapsedTime, sessionStart) {
+    function modificationToDB(db) {
+        const obj = new Session(elapsedTime, sessionStart);
 
         const tx = db.transaction([storeName], "readwrite")
         const store = tx.objectStore(storeName);
 
         const addReq = store.add(obj);
 
-        addReq.onsuccess = ()=>{
+        addReq.onsuccess = () => {
             console.log("A session has been added successfully.")
         }
-        addReq.onerror = (event)=>{
+        addReq.onerror = (event) => {
             console.error(`Add session request encounters error: ${event.error}`)
         }
 
-        tx.oncomplete = ()=>{db.close()}
+        tx.oncomplete = () => { db.close() }
     }
     openDB(modificationToDB)
 }
 
-function clearDataInDB(){
-    function clearAll(db){
-        const tx = db.transaction([storeName], "readonly")
+function clearDataInDB() {
+    function clearAll(db) {
+        const tx = db.transaction([storeName], "readwrite")
         const store = tx.objectStore(storeName);
         const clearReq = store.clear();
 
-        clearReq.onsuccess = ()=>console.log("All sessions have been clear.")
-        clearReq.onerror = (event)=>console.error(`Clear sessions encounters error: ${event.error}`)
-        tx.oncomplete = ()=>db.close()
+        clearReq.onsuccess = () => console.log("All sessions have been clear.")
+        clearReq.onerror = (event) => console.error(`Clear sessions encounters error: ${event.error}`)
+        tx.oncomplete = () => db.close()
     }
     openDB(clearAll)
 }
 
-//needs to be in YYYYMMDD
-function getHoursPWeek(date){
-    
+//needs to be in YYYY-MM-DD format as a string
+function queryWeekHours(startStr, endStr){
+    const keyRangeValue = IDBKeyRange.bound(startStr,endStr);
+    let result = 0;
+    function functionToDB(db){
+        const tx = db.transaction([storeName])
+        const store = tx.objectStore(storeName);
+
+        store.openCursor(keyRangeValue).onsuccess = (event)=>{
+            const cursor = event.target.result;
+            if(cursor){
+                const obj = cursor.value;
+                result += obj.elapsedTime;
+                cursor.continue()
+            }
+            console.log()
+        }
+        
+    }
+}
+function getHoursPWeek(dateStr) {
+    //define a week as from last Sunday to the current Saturday
+    const time = Date.parse(dateStr);
+    const target = new Date(time);
+    const thatSunday = new Date(time);
+    const thatSaturday = new Date(time);
+
+    const dayInWeek = target.getDay();
+    thatSunday.setDate(target.getDate() - dayInWeek)
+    //target.getDate() gets the target's date in the month
+    thatSaturday.setDate(target.getDate() + (6 - dayInWeek))
+
+    //setting up query range
+    const sunStr = thatSunday.toISOString().split('T')[0]
+    const satStr = thatSaturday.toISOString().split('T')[0]
+
+    //making query
+
 }
 
 let sessionStart;
-startBtn.addEventListener("click", ()=>{
-    if(isPause && !resumed){
+startBtn.addEventListener("click", () => {
+    if (isPause && !resumed) {
         isPause = false;
         startTime = Date.now();
         InitialTime = startTime;
         sessionStart = startTime;
-        const startDTString = `Start Date & Time: ${new 
-        Date(InitialTime).toLocaleString(undefined,options)}`;
+        const startDTString = `Start Date & Time: ${new
+            Date(InitialTime).toLocaleString(undefined, options)}`;
         startDT.textContent = startDTString;
         endDT.textContent = "End Date & Time: ";
         resultDisplay.textContent = "Elapsed Time of This Session: ";
@@ -131,31 +166,31 @@ startBtn.addEventListener("click", ()=>{
     }
 })
 
-pauseBtn.addEventListener("click", ()=>{
-    if(!isPause){//stopwatch running
+pauseBtn.addEventListener("click", () => {
+    if (!isPause) {//stopwatch running
         pauseBtn.textContent = "Resume";
         isPause = true;
-        elapsedTime = Date.now()-startTime;
+        elapsedTime = Date.now() - startTime;
         clearInterval(IntervalID)
-    }else{//stopwatch pausing
+    } else {//stopwatch pausing
         pauseBtn.textContent = "Pause";
         resumed = true;
         accumulated = elapsedTime;
         startTime = Date.now();
-        IntervalID = setInterval(updateTime,100)
+        IntervalID = setInterval(updateTime, 100)
     }
 })
 
-endBtn.addEventListener("click", ()=>{
+endBtn.addEventListener("click", () => {
     isPause = true;
     resumed = false;
     clearInterval(IntervalID);
 
     EndTime = Date.now();
     const endDTString = `End Date & Time: ${new Date(EndTime)
-    .toLocaleString(undefined,options)}`;
+        .toLocaleString(undefined, options)}`;
     endDT.textContent = endDTString;
-    displayInHourMinSec(elapsedTime,resultDisplay,'Elapsed Time of This Session: ')
+    displayInHourMinSec(elapsedTime, resultDisplay, 'Elapsed Time of This Session: ')
 
     // addSessiontoDB(elapsedTime,sessionStart);
 
@@ -169,26 +204,26 @@ endBtn.addEventListener("click", ()=>{
     timeDisplay.textContent = "00:00:00";
 })
 
-function updateTime(){
-    if(resumed){
-        elapsedTime = accumulated + (Date.now()-startTime);
-    }else{
-        elapsedTime = Date.now()-startTime;
+function updateTime() {
+    if (resumed) {
+        elapsedTime = accumulated + (Date.now() - startTime);
+    } else {
+        elapsedTime = Date.now() - startTime;
     }
-    displayInHourMinSec(elapsedTime,timeDisplay);
+    displayInHourMinSec(elapsedTime, timeDisplay);
 }
 
-function displayInHourMinSec(elapsedTime, target, customizedMes=''){
-    secs = Math.floor((elapsedTime/1000) %60);
-    mins = Math.floor((elapsedTime/(1000*60))%60);
-    hrs = Math.floor(elapsedTime/(1000*60*60));
-    
+function displayInHourMinSec(elapsedTime, target, customizedMes = '') {
+    secs = Math.floor((elapsedTime / 1000) % 60);
+    mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    hrs = Math.floor(elapsedTime / (1000 * 60 * 60));
+
     secs = pad(secs)
     mins = pad(mins)
     hrs = pad(hrs)
     target.textContent = `${customizedMes}${hrs}:${mins}:${secs}`;
 
-    function pad(unit){
-        return (('0')+unit).length > 2? unit:('0')+unit 
+    function pad(unit) {
+        return (('0') + unit).length > 2 ? unit : ('0') + unit
     }
 }
